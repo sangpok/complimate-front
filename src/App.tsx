4e86a2c3cc59c20bd2fd7fe652a2f70c3e2055fe
sangpok <48979587+sangpok@users.noreply.github.com>
2023-09-26 20:31:26 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+const App = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100dvw',
+        height: '100dvh',
+      }}
+    >
+      <p
+        style={{
+          fontSize: '2rem',
+          fontWeight: 'bolder',
+        }}
+      >
+        Hi, This is CompliMate
+      </p>
+      <p>스켈레톤 서버 테스트 중입니다.</p>
+    </div>
+  );
+};
+
+export default App;
